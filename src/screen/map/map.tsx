ce05b5c544e21a0186996f87f5474bd4023ab2cc
Jay Pan <lb0104672@gmail.com>
2022-07-27 17:04:37 +0800

    feat: add google place search functionality

--- src/screen/map/map.tsx
+++ src/screen/map/map.tsx
@@ -1,13 +1,66 @@
 import React, { FC } from 'react';
-import { View } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
-import { styles } from './styles';
+import { styles, googlePlacesAutocompleteStyles } from './styles';
+
+const { height, width } = Dimensions.get('window');
+const LATITUDE = 23.6978; // Taiwan
+const LONGITUDE = 120.9605;
+const LATITUDE_DELTA = 5;
+const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
 
 export const Map: FC = () => {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} provider={PROVIDER_GOOGLE} showsUserLocation />
+      <GooglePlacesAutocomplete
+        isRowScrollable={false}
+        styles={googlePlacesAutocompleteStyles}
+        placeholder="Search"
+        minLength={1}
+        fetchDetails
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(details);
+          // const placeId = data.place_id;
+          // console.log(details?.geometry, details?.formatted_address, details?.name, details?.url);
+        }}
+        onFail={(error) => console.error(error)}
+        query={{
+          key: 'AIzaSyDAc6Gjizga1K8N0witdqpHKX3RAddK9n8',
+          language: 'zh-TW',
+        }}
+        renderDescription={(data) => data.structured_formatting.main_text}
+        debounce={300}
+        renderRow={(rowData) => {
+          const title = rowData.structured_formatting.main_text;
+          const address = rowData.structured_formatting.secondary_text;
+
+          return (
+            <View>
+              <Text numberOfLines={1} ellipsizeMode="tail">
+                {title}
+              </Text>
+              <Text numberOfLines={1} ellipsizeMode="tail" style={styles.address}>
+                {address}
+              </Text>
+            </View>
+          );
+        }}
+      />
+      <MapView
+        initialRegion={{
+          latitude: LATITUDE,
+          longitude: LONGITUDE,
+          latitudeDelta: 5,
+          longitudeDelta: LONGITUDE_DELTA,
+        }}
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        showsMyLocationButton
+      />
     </View>
   );
 };
--- src/screen/map/styles.ts
+++ src/screen/map/styles.ts
@@ -1,14 +1,45 @@
 import { StyleSheet, Dimensions } from 'react-native';
+import Constants from 'expo-constants';
+import { Styles } from 'react-native-google-places-autocomplete';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   map: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    height: Dimensions.get('window').height - 88,
+  },
+  address: {
+    color: '#777',
   },
 });
+
+const textInputOffset = Constants.statusBarHeight + 10;
+
+export const googlePlacesAutocompleteStyles: Partial<Styles> = {
+  container: {
+    position: 'absolute',
+    top: textInputOffset,
+    left: 32,
+    right: 32,
+    height: 40,
+    zIndex: 999,
+  },
+  textInput: {
+    borderColor: '#eee',
+    borderWidth: 1,
+  },
+  listView: {
+    backgroundColor: '#fff',
+    borderColor: '#eee',
+    borderWidth: 1,
+    borderRadius: 2,
+    position: 'absolute',
+    top: textInputOffset,
+  },
+  poweredContainer: {
+    display: 'none',
+  },
+};
